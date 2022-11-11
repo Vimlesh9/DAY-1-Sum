87194926b26c7b64f6c518d003aa1f711342cf8d
Vimlesh Yadav <117926668+Vimlesh9@users.noreply.github.com>
2022-11-11 12:30:13 +0530

    Add files via upload

---
+++ expression.c
@@ -0,0 +1,5 @@
+#include<stdio.h>
+float main() {
+    printf("The value of Expression is %.2f",(3.0/8)-2);
+    return 0;
+} 
---
+++ sum.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main() {
+    int a,b;
+    printf("Enter the value of a:");
+    scanf("%d",&a);
+    printf("\nEnter the value of b:");
+    scanf("%d",&b);
+    printf("sum of a & b is % d",a+b);
+    return 0;
+    
+} 
